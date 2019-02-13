acc2f916fa941dee42d727fc465150f2334a80e4
gudrunivans <gudruni17@ru.is>
2019-02-13 14:28:41 +0000

    Setting up large Assignment 2

---
+++ LargeAssignment2/drawio.js
@@ -0,0 +1,56 @@
+window.drawio = {
+    shapes: [],
+    selectedShape: 'rectangle',
+    ctx: document.getElementById('my-canvas').getContext('2d'),
+    ctx: document.getElementById('my-canvas'),
+    selectedElement: null,
+    availableShapes: {
+        RECTANGLE: 'rectangle'
+    }
+};
+
+$(function (){
+
+    function drawCanvas() {
+        if(drawio.selectedElement) {
+            drawio.selectedElement.render();
+        }
+        for(var i = 0; i < drawio.shapes.length; i++) {
+            drawio.shapes[i].render();
+        }
+    };
+
+    $('.icon').on('click', function() {
+        $('.icon').removeClass('selected');
+        $(this).addClass('selected');
+        drawio.selectedShape = $(this).data('shape');
+    });
+
+    $('#my-canvas').on('mousedown', function (mouseEvent){
+        switch (drawio.selectedShape) {
+            case drawio.availableShapes.RECTANGLE:
+            drawio.selectedElement = new Rectangle({ x: mouseEvent.offsetX, y: mouseEvent.offset}, 0, 0);
+            break;
+        }
+    });
+
+    $('#my-canvas').on('mousemove', function (mouseEvent) {
+        if(drawio.selectedElement){
+            drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
+            drawio.selectedElement.resize(mouseEvent.offsetx, mouseEvent.offsetY);
+            drawCanvas();
+        }
+    });
+
+    $('#my-canvas').on('mouseup', function () {
+        drawio.shapes.push(drawio.selectedElement);
+        drawio.selectedElement = null; 
+    });
+});
+
+
+
+
+
+
+
