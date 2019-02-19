984204859a0fc295f44fbb0e3050103bf8651dcb
unknown <hjorturjohann94@gmail.com>
2019-02-19 14:47:32 +0000

    Circle shape

--- LargeAssignment2/shapes.js
+++ LargeAssignment2/shapes.js
@@ -30,3 +30,22 @@ Rectangle.prototype.resize = function (x, y) {
     this.width = x - this.position.x;
     this.height = y - this.position.y;
 };
+
+//Circle Shape
+function Circle(position, radius) {
+    Shape.call(this, position);
+    this.radius = radius;
+};
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.render = function () {
+    drawio.ctx.beginPath();
+    drawio.ctx.arc(this.position.x, this.position.y, this.radius, 0, 2* Math.PI);
+    drawio.ctx.stroke();
+}
+
+Circle.prototype.resize = function (x, y) {
+    this.radius = Math.sqrt(Math.pow((x - this.position.x), 2) + Math.pow((y - this.position.y), 2));
+}
