54a4a0898d61a7f180b1d1634077e5ccb6d1ffdc
unknown <hjorturjohann94@gmail.com>
2019-02-19 18:13:45 +0000

    undo redo

--- LargeAssignment2/drawio.js
+++ LargeAssignment2/drawio.js
@@ -1,5 +1,6 @@
 window.drawio = {
     shapes: [],
+    deletedShapes: [],
     selectedShape: 'pen',
     canvas: document.getElementById('my-canvas'),
     ctx: document.getElementById('my-canvas').getContext('2d'),
@@ -12,7 +13,8 @@ window.drawio = {
         LINE: 'line'
     }
     //To do:
-    //Create a move "shape" that when selected will let you  move the object you have selected
+    //Create a move "shape" that when selected will let you move the object you have selected
+    //Filled boolean that will toggle shapes between stroke and fill?
 };
 
 $(function() {
@@ -27,67 +29,89 @@ $(function() {
         }
     }
 
-        $('.icon').on('click', function () {
-            $('.icon').removeClass('selected');
-            $(this).addClass('selected');
-            drawio.selectedShape = $(this).data('shape');
-        });
+    $('.icon').on('click', function () {
+        $('.icon').removeClass('selected');
+        $(this).addClass('selected');
+        drawio.selectedShape = $(this).data('shape');
+    });
 
-        
-        $('button').on('click', function(){
-            $('button').removeClass('selected');
-            $(this).addClass('selected');
-            //drawio
-        });
+    
+    $('button').on('click', function(){
+        $('button').removeClass('selected');
+        $(this).addClass('selected');
+        //drawio
+    });
 
-        $('#save').on('click', function(){
-            //Save array of shapes into localStorage
-            //Dont clear the canvas
-        });
+    $('#save').on('click', function(){
+        //Save array of shapes into localStorage
+        //Dont clear the canvas
+    });
 
-        $('#load').on('click', function(){
-            //Clear canvas
-            //Load currently selected localStorage object into canvas
-        });
-       
+    $('#load').on('click', function(){
+        //Clear canvas
+        //Load currently selected localStorage object into canvas
+    });
 
-        $('#my-canvas').on('mousedown', function (mouseEvent) {
-            switch (drawio.selectedShape) {
-                case drawio.availableShapes.PEN:
-                    drawio.selectedElement = new Pen( {x: mouseEvent.offsetX, y: mouseEvent.offsetY});
-                    break;
-                case drawio.availableShapes.RECTANGLE:
-                    drawio.selectedElement = new Rectangle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
-                    break;
-                case drawio.availableShapes.CIRCLE:
-                    drawio.selectedElement = new Circle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0);
-                    break;
-                case drawio.availableShapes.TEXT:
-                    drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY});
-                    break;
-                case drawio.availableShapes.LINE:
-                    drawio.selectedElement = new Line( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
-                    break;
-            }
-        });
+    $('#undo-btn').on('click', function(){
+        //Take newest shape from shapes array and push it into a deleted shapes array
+        if(drawio.shapes.length > 0) {
+            drawio.deletedShapes.push(drawio.shapes.pop());
+        }
+        console.log(drawio.shapes);
+        console.log(drawio.deletedShapes);
+        drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
+        drawCanvas();
+    });
 
-        $('#my-canvas').on('mousemove', function (mouseEvent) {
-            if(drawio.selectedElement) {
-                drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
-                drawio.selectedElement.resize(mouseEvent.offsetX, mouseEvent.offsetY);
-                drawCanvas();
-            }
-            //Need to consider the case when the move tool is selected
-        });
+    $('#redo-btn').on('click', function(){
+        //Take the newest hsape from deleted shapes and push it into the shapes array
+        if(drawio.deletedShapes.length > 0) {
+            drawio.shapes.push(drawio.deletedShapes.pop());
+        }
+        console.log(drawio.shapes);
+        console.log(drawio.deletedShapes);
+        drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
+        drawCanvas();
+    });
+    
 
-        $('#my-canvas').on('mouseup', function () {
-            //Check if selected element is null, we dont want to push null
-            if(drawio.selectedElement) {
-                drawio.shapes.push(drawio.selectedElement);
-            }
-            console.log(drawio.shapes);
-            drawio.selectedElement = null;
-        });
+    $('#my-canvas').on('mousedown', function (mouseEvent) {
+        switch (drawio.selectedShape) {
+            case drawio.availableShapes.PEN:
+                drawio.selectedElement = new Pen( {x: mouseEvent.offsetX, y: mouseEvent.offsetY});
+                break;
+            case drawio.availableShapes.RECTANGLE:
+                drawio.selectedElement = new Rectangle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
+                break;
+            case drawio.availableShapes.CIRCLE:
+                drawio.selectedElement = new Circle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0);
+                break;
+            case drawio.availableShapes.TEXT:
+                drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY});
+                break;
+            case drawio.availableShapes.LINE:
+                drawio.selectedElement = new Line( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
+                break;
+        }
+    });
+
+    $('#my-canvas').on('mousemove', function (mouseEvent) {
+        if(drawio.selectedElement) {
+            drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
+            drawio.selectedElement.resize(mouseEvent.offsetX, mouseEvent.offsetY);
+            drawCanvas();
+        }
+        //Need to consider the case when the move tool is selected
+    });
+
+    $('#my-canvas').on('mouseup', function () {
+        //Check if selected element is null, we dont want to push null
+        if(drawio.selectedElement) {
+            drawio.shapes.push(drawio.selectedElement);
+        }
+        console.log(drawio.shapes);
+        drawio.selectedElement = null;
+    });
 
 });
 
