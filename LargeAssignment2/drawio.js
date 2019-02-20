0e6f037f3274630962725562d33843b8ff29b534
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-02-20 13:57:56 +0000

    New feature started

--- LargeAssignment2/drawio.js
+++ LargeAssignment2/drawio.js
@@ -128,7 +128,7 @@ $(function() {
                 drawio.selectedElement = new Circle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, drawio.selectedColor);
                 break;
             case drawio.availableShapes.TEXT:
-                drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY}, drawio.selectedColor);
+                drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY});
                 break;
             case drawio.availableShapes.LINE:
                 drawio.selectedElement = new Line( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0, drawio.selectedColor);
