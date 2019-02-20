7f50671f8f7c888b6621ddcddb13dddadb14b8de
unknown <hjorturjohann94@gmail.com>
2019-02-20 14:33:19 +0000

    second argument for text added

--- LargeAssignment2/drawio.js
+++ LargeAssignment2/drawio.js
@@ -136,7 +136,7 @@ $(function() {
                 drawio.selectedElement = new Circle( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, drawio.selectedColor);
                 break;
             case drawio.availableShapes.TEXT:
-                drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY});
+                drawio.selectedElement = new Text({x: mouseEvent.offsetX, y: mouseEvent.offsetY}, null);
                 break;
             case drawio.availableShapes.LINE:
                 drawio.selectedElement = new Line( {x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0, drawio.selectedColor);
