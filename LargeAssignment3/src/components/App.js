d385d3802c14c647f3b22b1cc9f8f532ac82c970
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-03-01 22:51:53 +0000

    Large Assignment init, modified gitignore

---
+++ LargeAssignment3/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <p>Start working here!</p>
+};
+
+export default App;
---
+++ LargeAssignment3/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader"
+                },
+                {
+                    loader: "less-loader"
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader"
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.less', '.css']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Bubblify', template: './index.html', inject: 'body' })
+    ],
+    devServer: {
+        open: true,
+        compress: true,
+        historyApiFallback: true,
+        port: 3000
+    }
+};
