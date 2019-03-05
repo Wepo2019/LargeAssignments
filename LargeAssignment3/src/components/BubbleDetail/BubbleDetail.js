78db89013ea0a8ae146c2ce0cbab09ca60f95e82
unknown <hjorturjohann94@gmail.com>
2019-03-05 21:08:21 +0000

    comments

--- LargeAssignment3/src/components/BubbleDetail/BubbleDetail.js
+++ LargeAssignment3/src/components/BubbleDetail/BubbleDetail.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BubbleConsumer } from '../../context/BubbleContext';
 
+/*
+Ok, kannski viljum við ekki nota context til að fá allan listann og svo filterann hann í 1 item
+Super messed up ef við gerum bubble consumer í render þá virkar það en verður undefined þegar við reynum að returna html element með value
+*/
+
 class BubbleDetail extends React.Component {
     componentDidMount() {
         <BubbleConsumer>
